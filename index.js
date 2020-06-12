0273065030af3a8174a26da1e8940e85de8b4be4
learnwithssh <sshupforwork@gmail.com>
2020-06-12 18:40:14 +0530

    POC with Cheerio

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    cheerio = require('cheerio'),
+    axios = require('axios');
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(__dirname, express.static('public'))
+
+// Globals
+let title,
+    description,
+    learning,
+    image,
+    expandMessage,
+    content;
+
+
+app.get('/', (req, res) => {
+    res.redirect('/search');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search')
+});
+
+app.post('/submit', (req, res) => {
+
+    axios.get('https://www.udemy.com/course/practical-leadership/')
+        .then(function (response) {
+            // handle success
+            const $ = cheerio.load(response.data);
+            title = $('h1.clp-lead__title').html();
+            learning = $('.what-you-get__items')
+                        .find('.what-you-get__text')
+                        .contents().toArray()
+            description = $('div.clp-component-render div.description div.js-simple-collapse-inner').html();
+            image = $('.introduction-asset img').attr('src')
+            content = $('.curriculum-wrapper')
+                        .find('.section-title-text')
+                        .contents()
+                        .toArray()
+            expandMessage = $('.section-container--more-sections').find('span').html()
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+            res.render("result", 
+                {
+                    expandMessage:expandMessage,
+                    description: description, 
+                    learning:learning, 
+                    content: content, 
+                    title:title,
+                    image: image
+                })
+        });
+});
+
+app.listen(8080, () => console.log("Started..."));
---
+++ readme.md
@@ -0,0 +1,8 @@
+## To test locally:
+*Note: You must have node installed*
+
+1. Clone the repo using `git clone`
+2. Inside the local folder run `npm install`
+3. After installation is complete run `node index.js`
+4. Go to localhost:8080 in the browser
+5. Click **Search** (no need to enter anything in text field)
